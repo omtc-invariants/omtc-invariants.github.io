8ddff2abd1ca88160bc98e7072cd9d8483d91f76
DEADLYIMPACT <aakashshankar2005@gmail.com>
2025-09-08 01:12:14 +0530

    Sidebar enhancer

---
+++ script.js
@@ -0,0 +1,15 @@
+let hamburger = document.getElementById("hamburger");
+let shortSidenav = document.getElementById("short-sidenav");
+let closeit = document.getElementById("closeit");
+let longSidenav = document.getElementById("long-sidenav");
+let content = document.getElementById("content");
+
+hamburger.onclick = function() {
+    longSidenav.style.left = "0%";
+    // content.style.transform = 'translateX(15%)'
+}
+
+closeit.onclick = function() {
+    longSidenav.style.left = "-20%";
+    // content.style.transform = 'translateX(0%)'
+}
